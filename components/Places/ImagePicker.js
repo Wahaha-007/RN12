ee6937a77787b191546455c3dc7dab408ee4d8e0
Wahaha <b0464221@gmail.com>
2022-05-23 12:15:07 +0700

    Finish camera capture & preview

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,90 @@
+import { Alert, Button, View, Image, StyleSheet, Text } from 'react-native';
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from 'expo-image-picker';
+
+import { useState } from 'react';
+import { Colors } from '../../constants/colors';
+import OutlinedButton from '../UI/OutlinedButton';
+
+function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  // 1. Permission ( Management ), Need this step for iOS
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions!',
+        'You need to grant camera permissions to use this app.'
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  // 2. Take Photo ( Worker )
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    if (!image.cancelled) {
+      setPickedImage(image.uri); // String
+    }
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
+    </View>
+  );
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 4,
+  },
+});
--- myNote.md
+++ myNote.md
@@ -1,2 +1,6 @@
-1. Install Navigation
+1. Navigation
    npm install @react-navigation/native @react-navigation/native-stack
+
+2. Expo image picker
+   ref: https://docs.expo.dev/versions/v45.0.0/sdk/imagepicker/
+   expo install expo-image-picker
